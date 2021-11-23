22a1159065b7ac2fb83d61012833818686bf3ee4
TzuMinYang <sam1248105@gmail.com>
2021-11-23 20:54:40 +0800

    HW3, multiple thread在bubble sort的雙層迴圈中, 沒用mutex包住會有問題, 不知道原因, 打算換個寫法

--- HW3/0816147_MT_best.cpp
+++ HW3/0816147_MT_best.cpp
@@ -3,9 +3,10 @@
 #include <vector>
 #include <math.h>
 #include <pthread.h>
+#include <sys/types.h>
 
 using namespace std;
-#define MAXNUM 1000009
+#define MAXNUM 100009
 #define PARTITION_NUM 8
 #define THREAD_NUM 4
 
@@ -17,12 +18,20 @@ struct partition
 int count = -1, num[MAXNUM] = {0};
 vector<partition> sub_array(PARTITION_NUM * 2 - 1);
 
+pthread_mutex_t mutex1, mutex2;
+
 void *bubble_sort(void *input_tinfo)
 {
-    int *input = (int *)input_tinfo, temp;
-    for(int i = 0; i < input[1] - input[0] + 1; i++)
+    int *input = (int *)input_tinfo, beg = input[0], end = input[1], temp;
+    //printf("bubble1: %d %d\n", input[0], input[1]);
+    //printf("bubble1: %d %d\n", beg, end);
+    //for(int i = beg; i < beg + 20; i++) printf("%d ", num[i]);
+    //printf("\n");
+    pthread_mutex_unlock(&mutex1);
+
+    for(int i = 0; i < end - beg + 1; i++)
     {
-        for(int j = input[0]; j < input[1] - i; j++)
+        for(int j = beg; j < end - i; j++)
         {
             if(num[j] > num[j + 1])
             {
@@ -32,6 +41,9 @@ void *bubble_sort(void *input_tinfo)
             }
         }
     }
+    //printf("bubble2: %d %d\n", input[0], input[1]);
+    //pthread_mutex_unlock(&mutex);
+    //return NULL;  
     pthread_exit(NULL);
 } 
 
@@ -40,6 +52,8 @@ void *merge(void *input_tinfo)
     int *input = (int *)input_tinfo;
     int ori_idx = input[0], left_idx = 0, right_idx = 0, left_size = input[1] - input[0] + 1, right_size = input[3] - input[2] + 1;
     int left[left_size], right[right_size];
+
+    pthread_mutex_unlock(&mutex1);
 
     for(int i = input[0], j = 0; i <= input[1]; i++, j++) left[j] = num[i];
     for(int i = input[2], j = 0; i <= input[3]; i++, j++) right[j] = num[i];
@@ -53,6 +67,7 @@ void *merge(void *input_tinfo)
     while(left_idx < left_size) num[ori_idx++] = left[left_idx++];
     while(right_idx < right_size) num[ori_idx++] = right[right_idx++];
 
+    //return NULL;    
     pthread_exit(NULL);
 }
 
@@ -82,43 +97,56 @@ int main (int argc, const char * argv[])
     new         xoxoxox
     */
 
+    pthread_mutex_init(&mutex1, 0);
+    pthread_mutex_init(&mutex2, 0);
+
     int input_to_func[4];
     pthread_t thread_num[THREAD_NUM];
     for(int i = 0; i < PARTITION_NUM ; i++)
     {
+        pthread_mutex_lock(&mutex1);
+
         input_to_func[0] = sub_array[i].beg_pos;
         input_to_func[1] = sub_array[i].end_pos;
 
+        //printf("to bubble: %d %d\n", input_to_func[0], input_to_func[1]);
         pthread_create(&thread_num[i % THREAD_NUM], NULL, bubble_sort, (void *)input_to_func);
+        //bubble_sort((void *)input_to_func);
 
-        if(i != 0 && i % THREAD_NUM == 0)
-            for(int j = 0; j < PARTITION_NUM ; j++) 
+        if(THREAD_NUM == 1 || (i != 0 && i % THREAD_NUM == 0))
+            for(int j = 0; j < THREAD_NUM ; j++) 
                 pthread_join(thread_num[j], NULL);
     }
 
-    for(int j = 0; j < PARTITION_NUM ; j++) 
-        pthread_join(thread_num[j], NULL);
-    
+    for(int i = 0; i < PARTITION_NUM % THREAD_NUM ; i++) 
+        pthread_join(thread_num[i], NULL);
+
+    //printf("%d %d ", count, PARTITION_NUM);
+    //for(int i = 0; i < count; i++) printf("%d ", num[i]);
+
+
     int merge_num = PARTITION_NUM / 2, sub_array_idx = 0;
     while(merge_num >= 1)
     {
-        for(int i = 0; i < merge_num ; i ++)
+        for(int i = 0; i < merge_num ; i++)
         {
+            pthread_mutex_lock(&mutex1);
+
             input_to_func[0] = sub_array[sub_array_idx].beg_pos;
             input_to_func[1] = sub_array[sub_array_idx].end_pos;
             input_to_func[2] = sub_array[sub_array_idx + 1].beg_pos;
             input_to_func[3] = sub_array[sub_array_idx + 1].end_pos;
             pthread_create(&thread_num[i % THREAD_NUM], NULL, merge, (void *)input_to_func);
 
-            if(i != 0 && i % THREAD_NUM == 0)
-                for(int j = 0; j < PARTITION_NUM ; j++) 
+            if(THREAD_NUM == 1 || (i != 0 && i % THREAD_NUM == 0))
+                for(int j = 0; j < THREAD_NUM ; j++) 
                     pthread_join(thread_num[j], NULL);
 
             sub_array_idx += 2;
         }
 
-        for(int j = 0; j < PARTITION_NUM ; j++) 
-            pthread_join(thread_num[j], NULL);
+        for(int i = 0; i < merge_num % THREAD_NUM ; i++) 
+            pthread_join(thread_num[i], NULL);
 
         merge_num /= 2;
     }
--- HW3/0816147_ST.cpp
+++ HW3/0816147_ST.cpp
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <vector>
 #include <math.h>
+#include <pthread.h>
 
 using namespace std;
 #define MAXNUM 1000009
 #define PARTITION_NUM 8
+#define THREAD_NUM 4
 
 struct partition
 {
@@ -29,8 +31,10 @@ void *bubble_sort(void *input_tinfo)
                 num[j + 1] = temp;
             }
         }
-            
     }
+
+    return NULL;
+    //pthread_exit(NULL);
 } 
 
 void *merge(void *input_tinfo)
@@ -50,6 +54,9 @@ void *merge(void *input_tinfo)
 
     while(left_idx < left_size) num[ori_idx++] = left[left_idx++];
     while(right_idx < right_size) num[ori_idx++] = right[right_idx++];
+
+    return NULL;
+    //pthread_exit(NULL);
 }
 
 int main (int argc, const char * argv[]) 
@@ -77,22 +84,34 @@ int main (int argc, const char * argv[])
     old xxooxxooxxooxx
     new         xoxoxox
     */
+
     int input_to_func[4];
+    pthread_t thread_num[THREAD_NUM];
     for(int i = 0; i < PARTITION_NUM ; i++)
     {
         input_to_func[0] = sub_array[i].beg_pos;
         input_to_func[1] = sub_array[i].end_pos;
+
         bubble_sort((void *)input_to_func);
     }
-
-    for(int i = 0; i < PARTITION_NUM * 2 - 1 - 1 ; i += 2)
+    
+    int merge_num = PARTITION_NUM / 2, sub_array_idx = 0;
+    while(merge_num >= 1)
     {
-        input_to_func[0] = sub_array[i].beg_pos;
-        input_to_func[1] = sub_array[i].end_pos;
-        input_to_func[2] = sub_array[i + 1].beg_pos;
-        input_to_func[3] = sub_array[i + 1].end_pos;
-        merge((void *)input_to_func);
+        for(int i = 0; i < merge_num ; i ++)
+        {
+            input_to_func[0] = sub_array[sub_array_idx].beg_pos;
+            input_to_func[1] = sub_array[sub_array_idx].end_pos;
+            input_to_func[2] = sub_array[sub_array_idx + 1].beg_pos;
+            input_to_func[3] = sub_array[sub_array_idx + 1].end_pos;
+            merge((void *)input_to_func);
+
+            sub_array_idx += 2;
+        }
+
+        merge_num /= 2;
     }
+    
 
 	/* Use STDOUT (e.g. printf, cout) to output the sorted array */
 	for(int i = 0; i < count; i++) printf("%d ", num[i]);
